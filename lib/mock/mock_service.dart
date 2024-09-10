115ea98d8c85d2c6671f4d79c2ecc0a8244ab64b
GodsGeneral <davidshittu20@gmail.com>
2024-09-10 11:54:10 -0700

    created a new list for soft drinks

--- lib/mock/mock_service.dart
+++ lib/mock/mock_service.dart
@@ -72,9 +72,41 @@ class MockService with ChangeNotifier {
         weight: "10pcs per bunch",
         imageUrl: "assets/images/strawberry.jpg"),
   ];
+  static final List<ItemModel> _softDrinks = [
+    ItemModel(
+        name: "Sprite",
+        price: 7.99,
+        quantity: 1,
+        weight: "325ml",
+        imageUrl: "assets/images/pngfuel_1.png"),
+    ItemModel(
+        name: "Coke Diet",
+        price: 19.99,
+        quantity: 1,
+        weight: "325ml",
+        imageUrl: "assets/images/pngfuel_2.png"),
+    ItemModel(
+        name: "Coke Classic",
+        price: 18.99,
+        quantity: 1,
+        weight: "300",
+        imageUrl: "assets/images/pngfuel_3.png"),
+    ItemModel(
+        name: "Tomato Juice",
+        price: 18.99,
+        quantity: 1,
+        weight: "200ml",
+        imageUrl: "assets/images/pngfuel_4.png",),
+    ItemModel(
+        name: "Pepsi",
+        price: 12.99,
+        quantity: 1,
+        weight: "300ml",
+        imageUrl: "assets/images/pngfuel_5.png"),
+  ];
   static final List<ExploreItemModel> _explore = [
     ExploreItemModel(name: "Fruits & Vegetables", items: _items),
-    ExploreItemModel(name: "Beverages", items: _items),
+    ExploreItemModel(name: "Beverages", items: _softDrinks),
     ExploreItemModel(name: "Soft Drinks", items: _items),
     ExploreItemModel(name: "Alcohol", items: _items),
     ExploreItemModel(name: "Fishes", items: _items),
